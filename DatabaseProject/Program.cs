214f2f781072a5527e388055300d2e258cbfa014
esmanur-karatas <esmanurkaratas0@gmail.com>
2024-10-30 20:59:28 +0300

    DatabaseProject

---
+++ DatabaseProject/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ado.net
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi *****");
+            Console.WriteLine();
+            
+            string tableNumber;
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            
+            Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+            tableNumber = Console.ReadLine();
+            Console.WriteLine("------------------------------------");
+
+            //sql bağlantısı
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-08VSUMT; Initial Catalog=EgitimKampiDb; Integrated Security=True");
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Category", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            connection.Close();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+            #endregion
+            Console.Read();
+        }
+    }
+}
